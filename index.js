6df727120aafc3b968883600e9243ac414c22f69
unknown <deenakannan100@gmail.com>
2019-04-25 05:40:47 +0530

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-console.log(__dirname + '/index.html'   )
+console.log(__dirname + '/index.html'  )
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
